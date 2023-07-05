e6100852d3c973a0423718477e472c7c4a060655
Vladimir Lesnykh <v.a.lesnykh@gmail.com>
2023-07-05 17:27:26 +0300

    first commit

---
+++ EmojiMatchingGame/PlayBoardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlayBoardViewController.swift
+//  EmojiMatchingGame
+//
+//  Created by Vladimir Lesnykh on 05.07.2023.
+//
+
+import UIKit
+
+class PlayBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
